f263de02d8b05244ee81e8132ab269cd0ed069fe
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2019-05-04 23:20:02 +0100

    Update Structure

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -1,5 +1,9 @@
+const path = require("path");
 module.exports = require('./webpack.config-helper')({
-  isProduction: false,
-  devtool: 'cheap-eval-source-map',
-  port: 8081
+	isProduction: false,
+	devtool: 'cheap-eval-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, "src")
+	},
+	port: 8081
 });
--- webpack.config-helper.js
+++ webpack.config-helper.js
@@ -1,176 +1,194 @@
-'use strict';
+"use strict";
 
-const Path = require('path');
-const Webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractSASS = new ExtractTextPlugin('./[name].[hash].css');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const webpack = require('webpack');
+const Path = require("path");
+const Webpack = require("webpack");
+const Glob = require("glob");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractSASS = new ExtractTextPlugin("./[name].[hash].css");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
-const pages = require('./src/pages');
+const PATHS = {
+	src: Path.join(__dirname, "src"),
+	dest: Path.join(__dirname, "dist")
+};
+
+const pages = require("./src/scripts/pages");
 let renderedPages = [];
 for (let i = 0; i < pages.length; i++) {
-  let page = Object.assign({}, pages[i]);
-  renderedPages.push(
-    new HtmlWebpackPlugin({
-      template: page.template,
-      filename: page.output,
-      title: page.content.title,
-      description: page.content.description
-    })
-  );
+	let page = Object.assign({}, pages[i]);
+	renderedPages.push(
+		new HtmlWebpackPlugin({
+			template: page.template,
+			filename: page.output,
+			title: page.content.title,
+			description: page.content.description
+		})
+	);
 }
 
 module.exports = (options) => {
-  const dest = Path.join(__dirname, 'dist');
 
-  let webpackConfig = {
-    devtool: options.devtool,
-    entry: ['./src/app.js'],
-    output: {
-      path: dest,
-      filename: './assets/scripts/[name].[hash].js'
-    },
-    plugins: [
-      new Webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery',
-        Tether: 'tether',
-        'window.Tether': 'tether',
-        Popper: ['popper.js', 'default'],
-      }),
-      new CopyWebpackPlugin([
-        {from: './src/assets/images', to: './assets/images'}
-      ]),
-      new CopyWebpackPlugin([
-        {from: './src/assets/fonts', to: './assets/fonts'}
-      ]),
-      new Webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(options.isProduction ? 'production' : 'development')
-        }
-      })
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        },
-        {
-          test: /\.hbs$/,
-          loader: 'handlebars-loader',
-          query: {
-            helperDirs: [
-              Path.join(__dirname, 'src', 'helpers')
-            ],
-            partialDirs: [
-              Path.join(__dirname, 'src', 'layouts'),
-              Path.join(__dirname, 'src', 'components'),
-              Path.join(__dirname, 'src', 'pages')
-            ]
-          }
-        },
-        {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [{
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: './assets/fonts'
-            }
-          }]
-        },
-        {
-          test: /\.(gif|jpg|png)$/,
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: './assets/images'
-          }
-        }
-      ]
-    }
-  };
+	let webpackConfig = {
+		devtool: options.devtool,
+		entry: [Path.join(PATHS.src, "scripts", "script.js")],
+		output: {
+			path: PATHS.dest,
+			filename: "./scripts/[name].[hash].js"
+		},
+		plugins: [
+			new Webpack.ProvidePlugin({
+				$: "jquery",
+				jQuery: "jquery",
+				"window.jQuery": "jquery",
+				Tether: "tether",
+				"window.Tether": "tether",
+				Popper: ["popper.js", "default"],
+			}),
+			new CopyWebpackPlugin([{
+				from: Path.join(PATHS.src, "assets"),
+				to: "./assets"
+			}]),
+			new CopyWebpackPlugin([{
+				from: Path.join(PATHS.src, "favicon.ico"),
+				to: "./"
+			}]),
+			new Webpack.DefinePlugin({
+				"process.env": {
+					NODE_ENV: JSON.stringify(options.isProduction ? "production" : "development")
+				}
+			})
+		],
+		module: {
+			rules: [{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: "babel-loader"
+				},
+				{
+					test: /\.hbs$/,
+					loader: "handlebars-loader",
+					query: {
+						helperDirs: [Path.join(PATHS.src, "helpers")],
+						partialDirs: [
+							Path.join(PATHS.src, "layouts"),
+							Path.join(PATHS.src, "components"),
+							Path.join(PATHS.src, "pages")
+						]
+					}
+				},
+				{
+					test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+					exclude: [/images/],
+					use: [{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "./assets/fonts"
+						}
+					}]
+				},
+				{
+					test: /\.(png|jpg|jpeg|gif|svg)$/,
+					loader: "file-loader",
+					exclude: [/fonts/],
+					options: {
+						name: "[name].[ext]",
+						outputPath: "./assets/images"
+					}
+				},
+				{
+					test: /\.(ico)$/,
+					loader: "file-loader",
+					exclude: [/fonts/],
+					options: {
+						name: "[name].[ext]",
+						outputPath: "./"
+					}
+				}
+			]
+		}
+	};
 
-  if (options.isProduction) {
-    webpackConfig.entry = ['./src/app.js'];
+	if (options.isProduction) {
+		webpackConfig.plugins.push(
+			ExtractSASS,
+			new CleanWebpackPlugin(["dist"], {
+				verbose: true,
+				dry: false
+			}),
+			new PurgecssPlugin({
+				paths: Glob.sync(`${PATHS.src}/**/*`, {
+					nodir: true
+				})
+			})
+		);
 
-    webpackConfig.plugins.push(
-      ExtractSASS,
-      new CleanWebpackPlugin(['dist'], {
-        verbose: true,
-        dry: false
-      })
-    );
+		webpackConfig.module.rules.push({
+			test: /\.scss$/i,
+			use: ExtractSASS.extract(["css-loader", "sass-loader"])
+		}, {
+			test: /\.css$/i,
+			use: ExtractSASS.extract(["css-loader"])
+		});
 
-    webpackConfig.module.rules.push({
-      test: /\.scss$/i,
-      use: ExtractSASS.extract(['css-loader', 'sass-loader'])
-    }, {
-      test: /\.css$/i,
-      use: ExtractSASS.extract(['css-loader'])
-    });
+	} else {
+		webpackConfig.plugins.push(
+			new Webpack.HotModuleReplacementPlugin()
+		);
 
-  } else {
-    webpackConfig.plugins.push(
-      new Webpack.HotModuleReplacementPlugin()
-    );
+		webpackConfig.module.rules.push({
+			test: /\.scss$/i,
+			use: ["style-loader?sourceMap", "css-loader?sourceMap", "sass-loader?sourceMap"]
+		}, {
+			test: /\.css$/i,
+			use: ["style-loader", "css-loader"]
+		}, {
+			test: /\.js$/,
+			use: "eslint-loader",
+			exclude: /node_modules/
+		});
 
-    webpackConfig.module.rules.push({
-      test: /\.scss$/i,
-      use: ['style-loader?sourceMap', 'css-loader?sourceMap', 'sass-loader?sourceMap']
-    }, {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader']
-    }, {
-      test: /\.js$/,
-      use: 'eslint-loader',
-      exclude: /node_modules/
-    });
+		webpackConfig.devServer = {
+			port: options.port,
+			contentBase: PATHS.dest,
+			historyApiFallback: true,
+			compress: options.isProduction,
+			inline: !options.isProduction,
+			hot: !options.isProduction,
+			stats: {
+				chunks: false
+			}
+		};
 
-    webpackConfig.devServer = {
-      port: options.port,
-      contentBase: dest,
-      historyApiFallback: true,
-      compress: options.isProduction,
-      inline: !options.isProduction,
-      hot: !options.isProduction,
-      stats: {
-        chunks: false
-      }
-    };
+		// webpackConfig.plugins.push(
+		// 	new BrowserSyncPlugin({
+		// 		host: "localhost",
+		// 		port: 3001,
+		// 		proxy: "http://localhost:8081/",
+		// 		files: [{
+		// 			match: [
+		// 				"**/*.hbs"
+		// 			],
+		// 			fn: function (event, file) {
+		// 				if (event === "change" || event === "add" || event === "unlink") {
+		// 					const bs = require("browser-sync").get("bs-webpack-plugin");
+		// 					bs.reload();
+		// 				}
+		// 			}
+		// 		}]
+		// 	}, {
+		// 		reload: false
+		// 	})
+		// );
 
-    webpackConfig.plugins.push(
-      new BrowserSyncPlugin({
-        host: 'localhost',
-        port: 3001,
-        proxy: 'http://localhost:8081/',
-        files: [{
-          match: [
-            '**/*.hbs'
-          ],
-          fn: function (event, file) {
-            if (event === 'change' || event === 'add' || event === 'unlink') {
-              const bs = require('browser-sync').get('bs-webpack-plugin');
-              bs.reload();
-            }
-          }
-        }]
-      }, {
-        reload: false
-      })
-    );
+	}
 
-  }
+	webpackConfig.plugins = webpackConfig.plugins.concat(renderedPages);
 
-  webpackConfig.plugins = webpackConfig.plugins.concat(renderedPages);
-
-  return webpackConfig;
+	return webpackConfig;
 
 };
